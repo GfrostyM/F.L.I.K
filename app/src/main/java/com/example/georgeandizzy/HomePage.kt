7485a000a616b7b1ac7ec0234848c587d611e6be
GfrostyM <23177@students.mrgs.school.nz>
2026-05-21 10:45:40 +1200

    george home page link to schedule 21/05/26

--- app/src/main/java/com/example/georgeandizzy/HomePage.kt
+++ app/src/main/java/com/example/georgeandizzy/HomePage.kt
@@ -1,6 +1,9 @@
 package com.example.georgeandizzy
 
+import android.adservices.ondevicepersonalization.InferenceInput
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class HomePage : AppCompatActivity() {
     lateinit var titleUsername : TextView
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +23,17 @@ class HomePage : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         titleUsername = findViewById(R.id.textView4)
         val name = intent.getStringExtra("username")
         titleUsername.text = "Hi ${name ?: "User"}"
+
+        button = findViewById(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, Schedule::class.java)
+
+            startActivity(intent)
+
+        }
     }
 }
